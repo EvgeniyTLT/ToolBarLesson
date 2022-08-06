4b585f7c7f9413788997fb6ff532d7e055e06cca
EvgeniyTLT <Toy444@ya.ru>
2022-08-06 18:27:54 +0300

    add

--- app/src/main/java/com/example/toolbarlesson/MainActivity.kt
+++ app/src/main/java/com/example/toolbarlesson/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.toolbarlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Admin"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home)
+            return true
     }
 }
